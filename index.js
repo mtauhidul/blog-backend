c99a448c1854d20c4236be663df4ba12d45767ed
MIR TAUHIDUL ISLAM <mirtauhidulislam@MIRs-MacBook-Air.local>
2024-07-10 11:52:03 +0600

    stable code state with every component

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
@@ -19,19 +20,81 @@ mongoose.connect(mongoUrl);
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+morgan.token("body", (req) => JSON.stringify(req.body));
+const format =
+  ":method :url :status :res[content-length] - :response-time ms :body";
+app.use(morgan(format));
+
+app.get("/api/blogs", (req, res, next) => {
+  Blog.find({})
+    .then((blogs) => res.json(blogs))
+    .catch(next);
 });
 
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
+app.get("/api/blogs/:id", (req, res, next) => {
+  Blog.findById(req.params.id)
+    .then((blog) => {
+      if (blog) {
+        res.json(blog);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
+
+app.put("/api/blogs/:id", (req, res, next) => {
+  const { title, author, url, likes } = req.body;
+  const blog = { title, author, url, likes };
+
+  Blog.findByIdAndUpdate(req.params.id, blog, {
+    new: true,
+    runValidators: true,
+    context: "query",
+  })
+    .then((updatedBlog) => res.json(updatedBlog))
+    .catch(next);
+});
+
+app.delete("/api/blogs/:id", (req, res, next) => {
+  Blog.findByIdAndDelete(req.params.id)
+    .then(() => res.status(204).end())
+    .catch(next);
+});
+
+app.post("/api/blogs", (req, res, next) => {
+  const { title, author, url, likes } = req.body;
+
+  if (!title || !author || !url || likes === undefined) {
+    return res.status(400).json({ error: "Anything missing!" });
+  }
+
+  const blog = new Blog({ title, author, url, likes });
+  blog
+    .save()
+    .then((returnedBlog) => {
+      res.status(201).json(returnedBlog);
+    })
+    .catch((error) => next(error));
+});
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: "unknown endpoint" });
+};
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformed id!" });
+  } else if (error.name === "ValidationError") {
+    return res.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
