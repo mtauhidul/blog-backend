254f7c73c969ad49cb586ab643af516754d44dfa
MIR TAUHIDUL ISLAM <mirtauhidulislam@MIRs-MacBook-Air.local>
2024-07-13 12:12:50 +0600

    tested new blog post addition method and checked content equality of new blog

--- tests/api.test.js
+++ tests/api.test.js
@@ -33,21 +33,19 @@ const initialBlogs = [
     likes: 175,
   },
   {
-    title: "Introduction to TypeScript",
-    author: "Carol White",
-    url: "https://example.com/typescript-intro",
-    likes: 210,
+    title: "Learning GraphQL",
+    author: "Eve White",
+    url: "https://example.com/learning-graphql",
+    likes: 180,
   },
 ];
 
 beforeEach(async () => {
   await Blog.deleteMany({});
-
   const blogPromises = initialBlogs.map((blog) => {
     const blogObject = new Blog(blog);
     return blogObject.save();
   });
-
   await Promise.all(blogPromises);
 });
 
@@ -58,15 +56,40 @@ test("blogs are returned as json", async () => {
     .expect("Content-Type", /application\/json/);
 });
 
-test("there are five notes", async () => {
+test("there are five blogs", async () => {
   const response = await api.get("/api/blogs");
   assert.strictEqual(response.body.length, 5);
 });
 
-test.only("blogs unique identifier is id", async () => {
+test("blogs unique identifier is id", async () => {
   const response = await api.get("/api/blogs");
   const blog = response.body[0];
-  assert(blog.hasOwnProperty("id"));
+  assert.ok(blog.hasOwnProperty("id"));
+});
+
+test("test blog post method", async () => {
+  const newBlog = {
+    title: "Introduction to TypeScript",
+    author: "Carol White",
+    url: "https://example.com/typescript-intro",
+    likes: 210,
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const response = await api.get("/api/blogs");
+
+  const newlyAddedBlog = response.body.find(
+    (blog) => blog.title === "Introduction to TypeScript"
+  );
+
+  assert.strictEqual(response.body.length, initialBlogs.length + 1);
+  assert.ok(newlyAddedBlog);
+  assert.deepEqual(newlyAddedBlog, { ...newBlog, id: newlyAddedBlog.id });
 });
 
 after(async () => {
