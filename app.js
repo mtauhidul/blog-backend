f022ed1153b666414ca8f9bbbb573f699d67f38c
MIR TAUHIDUL ISLAM <mirtauhidulislam@MIRs-MacBook-Air.local>
2024-07-13 16:51:18 +0600

    user model created with get all and post router

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const config = require("./utils/config");
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const blogsRouter = require("./routes/blog");
+const blogsRouter = require("./controllers/blog");
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
+const usersRouter = require("./controllers/user");
 
 mongoose.set("strictQuery", false);
 
@@ -26,6 +27,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
